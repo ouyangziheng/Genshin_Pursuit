7ba48f8dac4e6f513352f655a909e42ad8fdd29a
ouyangziheng <541478026@qq.com>
2024-03-15 18:15:40 +0800

    此次更改加入了开始界面的图片

--- startscene.cpp
+++ startscene.cpp
@@ -11,26 +11,34 @@ StartScene::StartScene(QWidget *parent)
     //创建主要界面
     MainScene *mainScene = new MainScene(this);
 
+    // 设置背景图片
+    // 设置背景图片
+    QString imagePath = ":/background/1.png";// 设置背景图片路径
+
+    // 检查图片是否存在
+    if (QFile::exists(imagePath)) {
+        QPixmap backgroundImage(imagePath);// 加载背景图片
+        if (!backgroundImage.isNull()) {
+            backgroundImage = backgroundImage.scaled(this->size(), Qt::KeepAspectRatioByExpanding);// 调整图片尺寸以适应窗口大小
+            QPalette palette;
+            palette.setBrush(this->backgroundRole(), QBrush(backgroundImage));
+            this->setPalette(palette);
+        } else {
+            qDebug() << "Error: Failed to load background image";
+        }
+    } else {
+        qDebug() << "Error: Background image path does not exist";
+    }
+
     //开始按钮
     {
         QPushButton *startButton = new QPushButton(this);
-        startButton->setFixedSize(500, 80);
-        startButton->move(700, 800);
+        startButton->setFixedSize(400, 64);
+        startButton->move(670, 890);
         startButton->setText("开始游戏");
         connect(startButton, &QPushButton::clicked, [=]() {
             this->close();
             mainScene->show();
-        });
-    }
-
-    //退出按钮
-    {
-        QPushButton *exitButton = new QPushButton(this);
-        exitButton->setFixedSize(500, 80);
-        exitButton->move(700, 900);
-        exitButton->setText("退出游戏");
-        connect(exitButton, &QPushButton::clicked, [=]() {
-            this->close();
         });
     }
 }
